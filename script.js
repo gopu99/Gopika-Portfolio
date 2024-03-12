d4d4c06738beafe8d1ed8f5c0e816b75c289239c
Gopika Kanattil <47878362+gopu99@users.noreply.github.com>
2024-03-12 04:20:34 -0400

    Header Added

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('imageContainer').addEventListener('click', function () {
+    var image = document.getElementById('profileImage');
+    image.classList.toggle('enlarged');
+});
