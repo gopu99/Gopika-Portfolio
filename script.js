346c9c97bd91a414170f17fef4ae519ea6afcf77
Gopika Kanattil <47878362+gopu99@users.noreply.github.com>
2024-03-13 02:25:51 -0400

    Header updated

--- script.js
+++ script.js
@@ -1,4 +1,19 @@
-document.getElementById('imageContainer').addEventListener('click', function () {
-    var image = document.getElementById('profileImage');
-    image.classList.toggle('enlarged');
-});
+function enlargeImage() {
+    // Get the modal
+    var modal = document.getElementById("imageModal");
+    // Get the image and insert it inside the modal
+    var img = document.querySelector(".image-container img");
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+
+    // Optionally, you could include the text below the image like this:
+    captionText.innerHTML = img.alt + "<br><p style='color:#fff;'>Gopika Kanattil</p>";
+}
+
+function closeModal() {
+    var modal = document.getElementById("imageModal");
+    modal.style.display = "none";
+}
